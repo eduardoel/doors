d82cc8c812941cebc27df91038c09b92960a2d55
Eduardo Elly <eduardo_elly@hotmail.com>
2021-09-09 18:30:08 -0300

    Router

--- src/pages/game/[doors]/[haveGift].tsx
+++ src/pages/game/[doors]/[haveGift].tsx
@@ -1,11 +1,19 @@
-import styles from "../styles/Game.module.css"
-import { useState } from "react"
-import Door from "../components/Door"
-import { createDoors, updateDoors } from "../functions/doors"
+import styles from "../../../styles/Game.module.css"
+import { useEffect, useState } from "react"
+import Door from "../../../components/Door"
+import { createDoors, updateDoors } from "../../../functions/doors"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function game() {
-    const [doors, setDoors] = useState(createDoors(4,3))
+    const router = useRouter()
+    const [doors, setDoors] = useState([])
+
+    useEffect(() => {
+        const doors = +router.query.doors
+        const haveGift = +router.query.haveGift
+        setDoors(createDoors(doors, haveGift))
+    }, [router?.query])
 
     function renderDoors() {
       return doors.map(door => {
